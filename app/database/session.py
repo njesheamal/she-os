5dd32e941b24213ff758c8dd45ef2a51928fdf22
Amal <61441606+njesheamal@users.noreply.github.com>
2026-08-16 01:23:53 +0000

    feat:  add initital database migration

--- app/database/session.py
+++ app/database/session.py
@@ -2,7 +2,7 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = getenv("DATABASE_URL", "postgresql://she_os_user:she_os_password@localhost:5432/she_os_dev")
+DATABASE_URL = getenv("DATABASE_URL", "postgresql+psycopg://she_os_user:she_os_password@localhost:5432/she_os_dev")
 
 # For SQLite, set check_same_thread=False to allow usage with FastAPI's threaded
 # server. For other databases (Postgres, MySQL, etc.) no special connect args.
